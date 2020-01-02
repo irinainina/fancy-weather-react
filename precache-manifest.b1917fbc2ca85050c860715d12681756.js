7e03498731db4ca49e3a1047df5396a0479c8bdf
Irina <irbris@mail.ru>
2020-01-03 00:04:09 +0200

    Add files via upload

---
+++ precache-manifest.b1917fbc2ca85050c860715d12681756.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d19c51d675d921476d509d9436aac69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "256cf25cbcf00a238fc5",
+    "url": "/static/css/main.3cc47021.chunk.css"
+  },
+  {
+    "revision": "2b6c59c5bc6feb6295fc",
+    "url": "/static/js/2.358b842f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.358b842f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "256cf25cbcf00a238fc5",
+    "url": "/static/js/main.0b882038.chunk.js"
+  },
+  {
+    "revision": "0e09b5b9c852a25819f2",
+    "url": "/static/js/runtime-main.d4b93def.js"
+  },
+  {
+    "revision": "a34a420be1193527133f0a7c11ef1113",
+    "url": "/static/media/Montserrat.a34a420b.woff2"
+  },
+  {
+    "revision": "58fe2032a07491d722704318e649acba",
+    "url": "/static/media/MontserratBold.58fe2032.woff2"
+  },
+  {
+    "revision": "264964868419227491877e695b21785b",
+    "url": "/static/media/MontserratThin.26496486.woff2"
+  },
+  {
+    "revision": "e562d5c21725724178d235bdd425d403",
+    "url": "/static/media/arrow-down.e562d5c2.svg"
+  },
+  {
+    "revision": "15f3550269da93549f77d7a344b87c1c",
+    "url": "/static/media/bg.15f35502.jpg"
+  },
+  {
+    "revision": "da173859d1cffbb25f50ed1aad6b4320",
+    "url": "/static/media/bird-cells.da173859.svg"
+  },
+  {
+    "revision": "0602cdc1240c860c6c6f1bd9bd25c337",
+    "url": "/static/media/mic.0602cdc1.svg"
+  },
+  {
+    "revision": "a24000382e380e72e67c27927985eff2",
+    "url": "/static/media/pause.a2400038.svg"
+  },
+  {
+    "revision": "633cd801494a1497cc010bad5986b480",
+    "url": "/static/media/play.633cd801.svg"
+  },
+  {
+    "revision": "311126fd72c3de73d617bb9dc14e6a1b",
+    "url": "/static/media/reload-image.311126fd.svg"
+  }
+]);
