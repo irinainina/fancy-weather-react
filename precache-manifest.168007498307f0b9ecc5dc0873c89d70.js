83212bd69cbeb9b7eafe861ab0c1621af09584a4
irinainina <irbris@mail.ru>
2022-04-05 00:08:15 +0300

    Updates

---
+++ precache-manifest.168007498307f0b9ecc5dc0873c89d70.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e958a8594e9676ccf8959e398be6d52",
+    "url": "/fancy-weather-react/index.html"
+  },
+  {
+    "revision": "e8f40f7f7acf08566ef7",
+    "url": "/fancy-weather-react/static/css/main.a35123fe.chunk.css"
+  },
+  {
+    "revision": "2994acd05555b2e2d334",
+    "url": "/fancy-weather-react/static/js/2.ad750bf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fancy-weather-react/static/js/2.ad750bf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e8f40f7f7acf08566ef7",
+    "url": "/fancy-weather-react/static/js/main.ed339e12.chunk.js"
+  },
+  {
+    "revision": "2daa66af8094e16f11d3",
+    "url": "/fancy-weather-react/static/js/runtime-main.66370dc9.js"
+  },
+  {
+    "revision": "a34a420be1193527133f0a7c11ef1113",
+    "url": "/fancy-weather-react/static/media/Montserrat.a34a420b.woff2"
+  },
+  {
+    "revision": "58fe2032a07491d722704318e649acba",
+    "url": "/fancy-weather-react/static/media/MontserratBold.58fe2032.woff2"
+  },
+  {
+    "revision": "264964868419227491877e695b21785b",
+    "url": "/fancy-weather-react/static/media/MontserratThin.26496486.woff2"
+  },
+  {
+    "revision": "59235564c97f3ae2418ca146e5f12361",
+    "url": "/fancy-weather-react/static/media/arrow-down.59235564.svg"
+  },
+  {
+    "revision": "15f3550269da93549f77d7a344b87c1c",
+    "url": "/fancy-weather-react/static/media/bg.15f35502.jpg"
+  },
+  {
+    "revision": "205d2f1e28a6231825169378949a1b38",
+    "url": "/fancy-weather-react/static/media/bird-cells.205d2f1e.svg"
+  },
+  {
+    "revision": "0602cdc1240c860c6c6f1bd9bd25c337",
+    "url": "/fancy-weather-react/static/media/mic.0602cdc1.svg"
+  },
+  {
+    "revision": "7afcb943610fb4ea6ced9e02c7094a47",
+    "url": "/fancy-weather-react/static/media/pause.7afcb943.svg"
+  },
+  {
+    "revision": "ff7c0b0d907a5079b522bf3b7cb35b3b",
+    "url": "/fancy-weather-react/static/media/play.ff7c0b0d.svg"
+  },
+  {
+    "revision": "48935305db42864027f2f8b0f0941efd",
+    "url": "/fancy-weather-react/static/media/reload-image.48935305.svg"
+  }
+]);
